ef56abca0b956a4186e8e05acf031ced1a0e0b8c
Angelus Vincent <angelusvincent@gmail.com>
2026-02-06 15:49:45 +0100

    fixed the _1D_collision function, now all tests are passing

--- src/kinematics.cpp
+++ src/kinematics.cpp
@@ -2,19 +2,40 @@
 #include "../include/kinematics.hpp"
 #include "units.hpp"
 
+typedef struct new_velocity {
+  si::velocity_t v1{};
+  si::velocity_t v2{};
+  new_velocity() = default;
+  new_velocity(si::velocity_t _v1, si::velocity_t _v2) {
+    v1 = _v1;
+    v2 = _v2;
+  }
+  new_velocity& operator=(new_velocity v) {
+    this->v1 = v.v1;
+    this->v2 = v.v2;
+    return *this;
+  }
+} new_velocity;
+
 void kinematics::Collision::_1D_collision(si::Particle *p1, si::Particle *p2) {
-  auto new_velocity = [](si::Particle *_p1, si::Particle *_p2) {
-    return 2*((_p1->mass() * _p1->velocity()) + (_p2->mass() * _p2->velocity()))/
-      (_p1->mass() + _p2->mass()); 
+  auto after_collision = [p1, p2]() {
+    return new_velocity {
+      2 * ((p1->mass() * p1->velocity() + p2->mass() * p2->velocity())
+          /(p1->mass() + p2->mass())) - p1->velocity(),
+      2 * ((p1->mass() * p1->velocity() + p2->mass() * p2->velocity())
+          /(p1->mass() + p2->mass())) - p2->velocity()
+    };
   };
 
-  auto new_ekin = [](si::Particle *p) {
+  new_velocity res = after_collision();
+
+  auto new_momentum = [](si::Particle *p) {
     return p->mass() * p->velocity();
   };
   
-  p1->set<si::velocity_t>() = new_velocity(p1, p2);
-  p2->set<si::velocity_t>() = new_velocity(p2, p1);
+  p1->set<si::velocity_t>() = res.v1;
+  p2->set<si::velocity_t>() = res.v2;
 
-  p1->set<si::momentum_t>() = new_ekin(p1);
-  p2->set<si::momentum_t>() = new_ekin(p2);
+  p1->set<si::momentum_t>() = new_momentum(p1);
+  p2->set<si::momentum_t>() = new_momentum(p2);
 }
